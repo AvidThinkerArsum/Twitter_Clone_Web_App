f9b92e2158fed195db83048a8b925a9276cfe440
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2024-05-09 13:33:18 -0700

    final styling

--- load_tweets.sh
+++ load_tweets.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-    python3 load_tweets.py --db=postgresql://postgres:pass@localhost:1620 --user_rows=50
+    python3 load_tweets.py --db=postgresql://postgres:pass@localhost:1620 --user_rows=1000000
